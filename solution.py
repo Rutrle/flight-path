eae2508387e66d96e5acdff00cc5a5bfdd702627
Jakub <rutrle@seznam.cz>
2021-11-16 17:32:38 +0100

    first functional version

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import json
 
 
 def read_flight_data(dst):
@@ -10,14 +11,14 @@ def read_flight_data(dst):
 
         for row in reader:
             flight={}
-            flight['number'] = row['flight_no']
+            flight['flight_no'] = row['flight_no']
             flight['origin'] = row['origin']
             flight['destination'] = row['destination']
             flight['departure'] = datetime.datetime.fromisoformat(row['departure'])
             flight['arrival'] = datetime.datetime.fromisoformat(row['arrival'])
-            flight['base_price'] = row['base_price']
-            flight['bag_price'] = row['bag_price']
-            flight['bags_allowed'] = row['bags_allowed']
+            flight['base_price'] = float(row['base_price'])
+            flight['bag_price'] = float(row['bag_price'])
+            flight['bags_allowed'] = int(row['bags_allowed'])
 
             flights.setdefault(flight['origin'],[])
 
@@ -49,41 +50,84 @@ def construct_graph(flights):
     '''
     return flight_graph    
 
-def find_all_paths(graph, src, dst,flight_data):
+def find_all_paths(src, dst,flight_data, graph):
 
-
-    def _inner_find_all_paths(graph, src, dst,arrival, current_path,flight_data):
+    def _inner_find_all_paths(graph, inbound_flight, dst, current_path,flight_data, current_visited,index):
     
-        current_path.append(src)
-        if src == dst:
+        current_path.append((inbound_flight))
+        current_visited.append(inbound_flight['destination'])
+        airport_path.append([inbound_flight['destination'],index])
+
+        if inbound_flight['destination'] == dst:
             finished_paths.append(tuple(current_path))
+            airport_fin.append(tuple(airport_path))
+            #print(current_path)
         
         else:
-            for airport in graph[src]:
-                for i, flightpath in enumerate(flight_data[src]):
-                    print(i,':',flightpath)
-                    layover_time = (flightpath['departure'] - arrival)
+            #for airport in graph[inbound_flight['destination']]:
+            for i, new_flightpath in enumerate(flight_data[inbound_flight['destination']]):
 
-                    layover_OK = datetime.timedelta(hours=1) < layover_time < datetime.timedelta(hours=6)
+                layover_time = (new_flightpath['departure'] - inbound_flight['arrival'])
+                layover_OK = datetime.timedelta(hours=1) < layover_time < datetime.timedelta(hours=6)
 
-                if airport not in current_path and layover_OK:
-                    _inner_find_all_paths(graph, airport, dst,flightpath['arrival'], current_path,flight_data)
+                if new_flightpath['destination'] not in current_visited and layover_OK:
+                    _inner_find_all_paths(graph, new_flightpath, dst, current_path,flight_data, current_visited,i)
         
         current_path.pop()
+        current_visited.pop()
+        airport_path.pop()
 
-    current_path=[src]
+    current_path=[]
+    current_visited = [src]
     finished_paths = []
+    airport_path = []   #to be deleted
+    airport_fin=[]
 
-    for airport in graph[src]:
-        for i, flightpath in enumerate(flight_data[src]):
-            _inner_find_all_paths(graph, flightpath['destination'], dst, flightpath['arrival'] , current_path,flight_data)
+    
+    for i, new_flightpath in enumerate(flight_data[src]):
+        _inner_find_all_paths(graph, new_flightpath, dst, current_path,flight_data, current_visited,i)
+
     print(finished_paths)
+    print(airport_fin)
+    return finished_paths
+
+def convert_to_JSON(flight_paths):
+    json_export=[]
+    for flight_path in flight_paths:
+        bags_allowed = float('inf')
+        current_flight={}
+        flights=[]
+        total_price = 0
+        travel_time = (flight_path[-1]['arrival']-flight_path[0]['departure'])
+        for flight in flight_path:
+            flight['departure'] = flight['departure'].isoformat()
+            flight['arrival'] = flight['arrival'].isoformat()
+
+
+            flights.append(flight)
+            total_price = total_price + flight["base_price"]
+
+            bags_allowed = min(bags_allowed,flight["bags_allowed"])
+
+        current_flight = {
+            "flights":flights,
+            "bags_allowed":bags_allowed,
+            "destination" :flight_path[-1]['destination'],
+            'origin':flight_path[0]['origin'],
+            'total_price':total_price,
+            'travel_time':str(travel_time)
+        }
+        
+        json_export.append(current_flight)
+    print(json.dumps(json_export, indent=4))
 
 if __name__ == '__main__':
     flight_data = read_flight_data('example/example0.csv')
-    print(flight_data)
+    #print(flight_data)
     flight_graph = construct_graph(flight_data)
-    print(flight_graph)
-    find_all_paths(flight_graph, 'WIW','RFZ',flight_data)
+    #print(flight_graph)
+    possible_paths = find_all_paths('ECV','RFZ',flight_data,flight_graph)
+
+    convert_to_JSON(possible_paths)
 
 
